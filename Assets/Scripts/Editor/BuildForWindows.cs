4add5dbffe04dab8486c09d30218a911f13270f7
wisockijunior <renato@wisockijunior.com.br>
2020-11-13 10:07:52 -0300

    3.2 - send the platform and the environment as arguments
    send the platform and the environment as arguments to the batch file

--- Assets/Scripts/Editor/BuildForWindows.cs
+++ Assets/Scripts/Editor/BuildForWindows.cs
@@ -7,18 +7,13 @@ using WisockiSampleCompileWindow;
 
 public class BuildForWindows : MonoBehaviour
 {
-    //// Start is called before the first frame update
-    //void Start()
-    //{
 
-    //}
-
-    //// Update is called once per frame
-    //void Update()
-    //{
-
-    //}
-
+    /*
+     * 3.2. Build script
+        Create a batch or shell script that receives the platform and the environment as
+        arguments, and outputs the final build. Android and Windows support is enough for the scope of
+        this test. You can offer any other arguments you want to increase the script’s flexibility.
+     */
     public enum targetPipaPlatform { android, windows };
 
 
@@ -47,10 +42,38 @@ public class BuildForWindows : MonoBehaviour
         string[] levels = new string[] { "Assets/Scenes/SampleScene.unity" };
 
         BuildTarget target = BuildTarget.StandaloneWindows;
+        
+        string arg1 = "targetPlatform";
+        string arg2 = "targetEnviroment";
+
+        string basePath = null; // not used here yet
+#if test
+        // LAN local network - IP fixo reservado: 192.168.25.241
+        basePath = @"http://192.168.25.241:8080/";// + assetBundleName;
+        basePath = @"http://192.168.25.242:8080/";// + assetBundleName;
+        arg2 = "test";
+#endif
+
+#if homologation
+
+        // using ddns for homologation
+
+        arg2 = "homologation";
+#endif
+
+#if production
+        
+        // production url
+        basePath = @"http://blocksxr.com/";
+
+        arg2 = "production";
+#endif
 
         if (targetPlatform == targetPipaPlatform.android)
         {
-            path = "bin/Android";            
+            path = "bin/Android";
+
+            arg1 = "android";
 
             target = BuildTarget.Android;
 
@@ -59,7 +82,9 @@ public class BuildForWindows : MonoBehaviour
         else
         if (targetPlatform == targetPipaPlatform.windows)
         {
-            path = "bin/Windows";            
+            path = "bin/Windows";
+
+            arg1 = "windows";
 
             target = BuildTarget.StandaloneWindows;
 
@@ -75,9 +100,11 @@ public class BuildForWindows : MonoBehaviour
         // Copy a file from the project folder to the build folder, alongside the built game.
         FileUtil.CopyFileOrDirectory("Assets/Templates/Readme.txt", path + "Readme.txt");
 
+        
+
         // Run the game (Process class from System.Diagnostics).
         Process proc = new Process();
-        proc.StartInfo.FileName = path + "/doit_upload.bat";
+        proc.StartInfo.FileName = path + "/doit_upload.bat "+arg1+" "+arg2;
         proc.Start();
     }
 
